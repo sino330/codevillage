105114e6920224dac00de4e0ad0807c2678f0423
sino330 <narutokozo330@gmail.com>
2021-07-01 15:50:09 +0900

    2021.7.1

---
+++ phase1/Phase1-format/js-easy/dice/main.js
@@ -0,0 +1,30 @@
+// const startBtn=document.getElementById('diceBtn');
+// const body=document.querySelector('body');
+// const dice=document.createElement('img');
+// dice.style.width='100px';
+// dice.style.height='100px';
+
+// let diceNum='./img/saikoro1.png';
+// dice.setAttribute("src", diceNum);
+// body.appendChild(dice);
+
+// let timer;
+
+// startBtn.addEventListener('click',function(){
+//     clearInterval(timer);
+//     timer=setInterval('random()',100);
+
+//     setTimeout(()=> {
+//       clearInterval(timer);
+//     }, 3000);
+// })
+
+// const random = function () {
+//   diceNum = `./img/saikoro${Math.floor(Math.random() * 6 + 1)}.png`;
+//   dice.setAttribute("src", diceNum);
+// };
+
+// 追加チャレンジ問題
+const startBtn=document.getElementsById('setPlayer1dice');
+const startBtn2=document.getElementsById('setPlayer2dice');
+
