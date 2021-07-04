70d86257f56b05ab6408de5c63ab3a85fb04db9f
sino330 <narutokozo330@gmail.com>
2021-07-04 19:20:58 +0900

    2021.7.4

--- phase1/Phase1-format/js-easy/dice/main.js
+++ phase1/Phase1-format/js-easy/dice/main.js
@@ -25,6 +25,49 @@
 // };
 
 // 追加チャレンジ問題
-const startBtn=document.getElementsById('setPlayer1dice');
-const startBtn2=document.getElementsById('setPlayer2dice');
 
+// 実行時の取り込み
+const player1Btn = document.getElementById("player1Btn");
+const player2Btn = document.getElementById("player2Btn");
+const setPlayer1dice = document.getElementById("setPlayer1dice");
+const setPlayer2dice = document.getElementById("setPlayer2dice");
+const result = document.getElementById("result");
+
+// timerの関数
+let player1Timer;
+let player2Timer;
+
+// diceNumの関数
+let player1Num = 1;
+let player2Num = 1;
+
+// diceのsrc属性挿入
+let player1Dice = `./img/saikoro${player1Num}.png`;
+setPlayer1dice.setAttribute("src", player1Dice);
+let player2Dice = `./img/saikoro${player2Num}.png`;
+setPlayer2dice.setAttribute("src", player2Dice);
+// ボタン
+player1Btn.addEventListener("click", function () {
+  clearInterval(player1Timer);
+  player1timer = setInterval("random1()", 100);
+  setTimeout(() => {
+    clearInterval(player1Timer);
+  }, 3000);
+});
+
+player2Btn.addEventListener("click", function () {
+  clearInterval(player2Timer);
+  player2timer = setInterval("random2()", 100);
+  setTimeout(() => {
+    clearInterval(player2Timer);
+  }, 3000);
+});
+// サイコロを振る
+const random1 = function () {
+  player1Dice = `.img/saikoro${Math.floor(Math.random() * 6 + 1)}.png`;
+  player1Dice.setAttribute('src',player1Num);
+};
+const random2 = function () {
+  player2Dice = `.img/saikoro${Math.floor(Math.random() * 6 + 1)}.png`;
+  player2Dice.setAttribute('src',player2Num);
+};
---
+++ phase1/Phase1-format/js-normal/hit-timer/main.js
@@ -0,0 +1,17 @@
+const confirmTime = document.getElementById("confirmTime");
+const startTimer = document.getElementById("startTimer");
+
+let sec=0;
+startTimer.addEventListener('click',function(){
+    sec=setInterval('cntUp()',1000)
+});
+
+confirmTime.addEventListener("click", function () {
+  if (sec === 0) {
+    alert("大正解^‐^");
+  } else if (sec < 20) {
+    alert(`まだ${sec}秒、、、再スタートだ`);
+  } else {
+    alert(`もう${sec}秒だ！再挑戦`);
+  }
+});
