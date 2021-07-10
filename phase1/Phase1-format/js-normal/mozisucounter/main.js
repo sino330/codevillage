039cc9291f594e119f1bf447f2517ca42b1750af
sino330 <narutokozo330@gmail.com>
2021-07-10 10:12:57 +0900

    2021.7/10

--- phase1/Phase1-format/js-normal/mozisucounter/main.js
+++ phase1/Phase1-format/js-normal/mozisucounter/main.js
@@ -3,20 +3,32 @@ const textCounter = document.getElementById("textCounter");
 const reset = document.getElementById("resetBtn");
 
 const character=400;
+let textarea;
+
 textCounter.textContent = `残り${character-input.value.length}文字`;
 
 input.addEventListener("keyup", function () {
     textCounter.textContent=`残り${character-input.value.length}文字`;
+    textarea=input.value;
 });
 
 reset.addEventListener('click',function(){
     textCounter.textContent = `残り${character}文字`;
+    textarea=""
 });
 
 //追加チャレンジ問題
 const iniDel=document.getElementById('iniDel');
 const endDel=document.getElementById('endDel');
 
-iniDel.addEventListener('click',function(){});
+iniDel.addEventListener('click',function(){
+    textarea=textarea.slice(1);
+    input.value=textarea;
+    textCounter.textContent=`残り${character-input.value.length}文字`;
+});
 
-endDel.addEventListener('click',function(){});
+endDel.addEventListener('click',function(){
+    textarea = textarea.slice(0,-1);
+    input.value = textarea;
+    textCounter.textContent = `残り${character - input.value.length}文字`;
+});
---
+++ phase1/Phase1-format/js-normal/slideshow/main.js
@@ -0,0 +1,32 @@
+const pushBtn = document.getElementById("pushBtn");
+const imgSection = document.getElementById("viewImg");
+const skipBtn=document.getElementById('skipBtn');
+const seasonNum=document.getElementById('seasonNum');
+
+const data = [
+  { img: "./img/slide1.png", text: "spring" },
+  { img: "./img/slide2.png", text: "summer" },
+  { img: "./img/slide3.png", text: "autumn" },
+  { img: "./img/slide4.png", text: "winter" },
+];
+
+let imgNum = 0;
+
+pushBtn.textContent = data[imgNum].text;
+imgSection.setAttribute("src", data[imgNum].img);
+
+pushBtn.addEventListener("click", function () {
+  if (imgNum === 3) {
+    imgNum = 0;
+  } else {
+    imgNum += 1;
+  }
+  imgSection.setAttribute("src", data[imgNum].img);
+  pushBtn.textContent = data[imgNum].text;
+});
+
+
+
+skipBtn.addEventListener('click',function(){
+    imgSection.setAttribute('src',data[seasonNum.value].img);
+});
