ad1acfd491306bd8df7e52703c7505028025ead4
sino330 <narutokozo330@gmail.com>
2021-08-16 12:32:14 +0900

    2021.8.16

--- phase1/Phase1-format/js-hard/bingosheet/main.js
+++ phase1/Phase1-format/js-hard/bingosheet/main.js
@@ -4,14 +4,46 @@ const row = 5;
 
 // tableの取得
 const table = document.getElementById("view");
-// 空の配列を作成
-tableSheet = [];
-//n列毎に1～15*nの数字を作成
-let setNum1 = Math.floor(15 * Math.random() + 1);
-let setNum2 = Math.floor(15 * Math.random() + 16);
-let setNum3 = Math.floor(15 * Math.random() + 31);
-let setNum4 = Math.floor(15 * Math.random() + 46);
-let setNum5 = Math.floor(15 * Math.random() + 61);
+//B列の数字重複しない数字の作成
+const sourceB = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+const numB = [];
+numB[0] = sourceB.splice(Math.floor(Math.random() * sourceB.length), 1)[0];
+numB[1] = sourceB.splice(Math.floor(Math.random() * sourceB.length), 1)[0];
+numB[2] = sourceB.splice(Math.floor(Math.random() * sourceB.length), 1)[0];
+numB[3] = sourceB.splice(Math.floor(Math.random() * sourceB.length), 1)[0];
+numB[4] = sourceB.splice(Math.floor(Math.random() * sourceB.length), 1)[0];
+//I列
+const sourceI = [16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30];
+const numI = [];
+numI[0] = sourceI.splice(Math.floor(Math.random() * sourceI.length), 1)[0];
+numI[1] = sourceI.splice(Math.floor(Math.random() * sourceI.length), 1)[0];
+numI[2] = sourceI.splice(Math.floor(Math.random() * sourceI.length), 1)[0];
+numI[3] = sourceI.splice(Math.floor(Math.random() * sourceI.length), 1)[0];
+numI[4] = sourceI.splice(Math.floor(Math.random() * sourceI.length), 1)[0];
+console.log(numI);
+//N列
+const sourceN = [31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45];
+const numN = [];
+numN[0] = sourceN.splice(Math.floor(Math.random() * sourceN.length), 1)[0];
+numN[1] = sourceN.splice(Math.floor(Math.random() * sourceN.length), 1)[0];
+numN[2] = sourceN.splice(Math.floor(Math.random() * sourceN.length), 1)[0];
+numN[3] = sourceN.splice(Math.floor(Math.random() * sourceN.length), 1)[0];
+//G列
+const sourceG = [46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60];
+const numG = [];
+numG[0] = sourceG.splice(Math.floor(Math.random() * sourceG.length), 1)[0];
+numG[1] = sourceG.splice(Math.floor(Math.random() * sourceG.length), 1)[0];
+numG[2] = sourceG.splice(Math.floor(Math.random() * sourceG.length), 1)[0];
+numG[3] = sourceG.splice(Math.floor(Math.random() * sourceG.length), 1)[0];
+numG[4] = sourceG.splice(Math.floor(Math.random() * sourceG.length), 1)[0];
+//O列
+const sourceO = [61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75];
+const numO = [];
+numO[0] = sourceO.splice(Math.floor(Math.random() * sourceO.length), 1)[0];
+numO[1] = sourceO.splice(Math.floor(Math.random() * sourceO.length), 1)[0];
+numO[2] = sourceO.splice(Math.floor(Math.random() * sourceO.length), 1)[0];
+numO[3] = sourceO.splice(Math.floor(Math.random() * sourceO.length), 1)[0];
+numO[4] = sourceO.splice(Math.floor(Math.random() * sourceO.length), 1)[0];
 
 // 関数指定
 function newSheet() {
@@ -46,44 +78,167 @@ function newSheet() {
       //それぞれのマスを配列
       else
         switch (n) {
+          //1列目の場合
           case 0:
-            //td内にtextContent追加
-            td.textContent = setNum1;
+            if (1 === i) {
+              td.textContent = numB[0];
+              td.setAttribute("id", "0");
+            } else if (2 === i) {
+              td.textContent = numB[1];
+              td.setAttribute("id", "1");
+            } else if (3 === i) {
+              td.textContent = numB[2];
+              td.setAttribute("id", "2");
+            } else if (4 === i) {
+              td.textContent = numB[3];
+              td.setAttribute("id", "3");
+            } else if (5 === i) {
+              td.textContent = numB[4];
+              td.setAttribute("id", "4");
+            }
             break;
           case 1:
-            td.textContent = setNum2;
+            if (1 === i) {
+              td.textContent = numI[0];
+              td.setAttribute("id", "5");
+            } else if (2 === i) {
+              td.textContent = numI[1];
+              td.setAttribute("id", "6");
+            } else if (3 === i) {
+              td.textContent = numI[2];
+              td.setAttribute("id", "7");
+            } else if (4 === i) {
+              td.textContent = numI[3];
+              td.setAttribute("id", "8");
+            } else if (5 === i) {
+              td.textContent = numI[4];
+              td.setAttribute("id", "9");
+            }
             break;
           //3行目、3列目はfree
           case 2:
+            if (1 === i) {
+              td.textContent = numN[0];
+              td.setAttribute("id", "10");
+            } else if (2 === i) {
+              td.textContent = numN[1];
+              td.setAttribute("id", "11");
+            } else if (4 === i) {
+              td.textContent = numN[2];
+              td.setAttribute("id", "12");
+            } else if (5 === i) {
+              td.textContent = numN[3];
+              td.setAttribute("id", "13");
+            }
             if (3 === i) {
               td.textContent = "free";
               td.setAttribute("class", "hit-num");
               break;
             }
-            td.textContent = setNum3;
             break;
           case 3:
-            td.textContent = setNum4;
+            if (1 === i) {
+              td.textContent = numG[0];
+              td.setAttribute("id", "14");
+            } else if (2 === i) {
+              td.textContent = numG[1];
+              td.setAttribute("id", "15");
+            } else if (3 === i) {
+              td.textContent = numG[2];
+              td.setAttribute("id", "16");
+            } else if (4 === i) {
+              td.textContent = numG[3];
+              td.setAttribute("id", "17");
+            } else if (5 === i) {
+              td.textContent = numG[4];
+              td.setAttribute("id", "18");
+            }
             break;
           case 4:
-            td.textContent = setNum5;
+            if (1 === i) {
+              td.textContent = numO[0];
+              td.setAttribute("id", "19");
+            } else if (2 === i) {
+              td.textContent = numO[1];
+              td.setAttribute("id", "20");
+            } else if (3 === i) {
+              td.textContent = numO[2];
+              td.setAttribute("id", "21");
+            } else if (4 === i) {
+              td.textContent = numO[3];
+              td.setAttribute("id", "22");
+            } else if (5 === i) {
+              td.textContent = numO[4];
+              td.setAttribute("id", "23");
+            }
             break;
         }
       tr.appendChild(td);
     }
   }
 }
-console.log(tableSheet);
 newSheet();
 
-//  tdそれぞれにidを指定
-// td.setAttribute("id",);
+//※重複しない数字を列毎に入力
+// setNum = function (i) {
+//   //1～15までの数字を作成
+//   const n = Math.floor(15 * Math.random() * i + 1);
+//   //入力する空配列の作成
+//   let sheet = [];
+//   //numBにrandomで取得したbの配列からindex数字にあった数字を取り出す
+//   //sheetに作成したnが含まれない場合
+//   if (!sheet.includes(n)) {
+//     //nをpush
+//     sheet.push(n);
+//     //nをtextcontentに表示
+//     td.textContent = n;
+//     //sheetに入れた配列の-1をidに表示
+//     td.setAttribute("id", sheet.length - 1);
+//   }
+// };
 
-//tdの中に数字を入力
-//td.textContent="";
+
+//n列毎に1～15*arr.lengthの数字を作成
+// let makeCol=function(base){
+//   //1～15の数字の配列を作成
+//   var arr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+//   var list=[];
+//   for(let i=1;i<line;i++){
+//     //1~15の乱数を生成
+//     var a=Math.floor(Math.random()*arr.length);
+//     list.push(arr[a]+base);
+//       //数字が重複しないように配列から削除
+//       arr.splice(a,1);
+//   }
+// }
+
+// let arrayB=array(5);
+//             arrayB[0]=Math.floor(Math.random()*15+1);
+//             td.textContent=arrayB[0];
+//             do{
+//               arrayB[1]=Math.floor(Math.random()*15+1);
+//             }while(arrayB[0]===arrayB[1]);
+//             do{
+//               arrayB[2]=Math.floor(Math.random()*15+1);
+//             while(arrayB[0]===arrayB[1]||arrayB[])
+//             }
+
+//※二次元配列例
+// const numbers = [
+//   [1, 2, 3, 4, 5],
+//   [11, 12, 13, 14, 15],
+//   [21, 22, null, 24, 25],
+//   [31, 32, 33, 34, 35],
+//   [41, 42, 43, 44, 45],
+// ];
+
+// console.log(numbers[2][0]);
 
 //チャレンジ問題
 //hitNumの読み込み
 const button = document.getElementById("hitNum");
 //hitNumを押したらイベントリスナー発火
-button.addEventListener("click", function () {});
+button.addEventListener("click", function () {
+  let e = Math.floor(75 * Math.random() + 1);
+  console.log(e);
+});
